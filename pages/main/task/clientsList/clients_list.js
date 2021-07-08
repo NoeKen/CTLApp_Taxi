82301b4bfd028dd5b91c43c32f6617e0fbdeae16
josephfeussi <feussilassi@gmail>
2021-07-08 08:56:28 +0200

    added a view for clients dispaly

--- pages/main/task/clientsList/clients_list.js
+++ pages/main/task/clientsList/clients_list.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { Text, View, Image,TouchableOpacity } from 'react-native';
+import styles from './style.js';
 
-const ClientsList = () => {
+const ClientsList = ({navigation}) => {
   return (
-    <SafeAreaView>
-        <Text>ClientsList is here</Text>
-    </SafeAreaView>
+    <View style={styles.maine}>
+      <Text style={styles.titre} >Mes Clients</Text>
+      <View style={styles.container}>
+        <View>
+          <Image
+            source={require('../../../../assets/images.jpeg')}
+            style={styles.backgroundImage}
+          />
+        </View>
+
+        <View style={styles.nameText}>
+          <Text style={styles.nameText}>Jo Owaldski</Text>
+          <Text style={styles.numText}>695233625</Text>
+        </View>
+      </View>
+    </View>
   );
 };
 
