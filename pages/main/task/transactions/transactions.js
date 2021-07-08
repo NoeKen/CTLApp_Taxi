39ad7d7adad485df8470ec2d769b4957247bb24f
josephfeussi <feussilassi@gmail>
2021-07-08 08:20:52 +0200

    view of the task page

--- pages/main/task/transactions/transactions.js
+++ pages/main/task/transactions/transactions.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import {Button, Text, View,Image,TouchableOpacity } from 'react-native';
+import styles from './style.js';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Transaction = () => {
+const Transaction = ({navigation}) => {
   return (
-    <SafeAreaView>
-        <Text>Transaction</Text>
-    </SafeAreaView>
+    <View style={styles.maine}>
+      <View style={styles.container}>
+        <View style={styles.taxiname}>
+          <Text style={styles.taxiText}>Active Taxi Name</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.taximg}
+          onPress={() => navigation.navigate('VehiculesList')}>
+          <Image
+            source={require('../../../../assets/images.jpeg')}
+            style={styles.backgroundImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        style={styles.destt}
+        onPress={() => navigation.navigate('#')}>
+        <Text>Ma Destination</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.transAct}
+        onPress={() => navigation.navigate('#')}>
+        <Icon name="home" size={20} color={{color: 'white'}} />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.clients}
+        onPress={() => navigation.navigate('ClientsList')}>
+        <Icon name="home" size={20} color={{color: 'white'}} />
+      </TouchableOpacity>
+
+    </View>
+
+    // <SafeAreaView>
+    //  </SafeAreaView>
   );
 };
 
